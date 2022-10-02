b248780b1685d62d5c6ce4cf2935206026862a6b
gowthamrazing <gowthamrazing@gmail.com>
2022-10-02 16:21:15 +0800

    Creating a Birthday Notes App with simple single view

--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var dayTextfield: UITextField!
+    
+    @IBOutlet weak var savedNameLabel: UILabel!
+    @IBOutlet weak var savedDayLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func saveDate(_ sender: Any) {
+    }
+    
 }
 
