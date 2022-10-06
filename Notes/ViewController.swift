1260d9af5540d0e3bf6cfdeeeb7b59137c5abd1e
gowthamrazing <gowthamrazing@gmail.com>
2022-10-06 20:57:17 +0800

    Save and Delete the data using UserDefaults in a single View App

--- Notes/ViewController.swift
+++ Notes/ViewController.swift
@@ -16,9 +16,50 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedDay = UserDefaults.standard.object(forKey: "birthday")
+        
+        //Casting - as? vs as!
+        //
+        
+        if let newName = storedName as? String {
+            savedNameLabel.text = "Name\(newName)"
+        }
+    
+        if let newBirthday = storedDay as? String {
+            savedDayLabel.text = "Birthday \(newBirthday)"
+        }
+        
     }
-
+    @IBAction func deleteClicked(_ sender: Any) {
+        
+        let storedName = UserDefaults.standard.object(forKey: "name")
+        let storedDate = UserDefaults.standard.object(forKey: "birthday")
+        
+        if (storedName as? String) != nil {
+            UserDefaults.standard.removeObject(forKey: "name")
+            
+            savedNameLabel.text = "Name:-"
+        }
+        
+        if (storedDate as? String) != nil {
+            UserDefaults.standard.removeObject(forKey: "birthday")
+            savedDayLabel.text = "Birthday:-"
+        }
+        
+        
+    }
+    
     @IBAction func saveDate(_ sender: Any) {
+        
+        UserDefaults.standard.set(nameTextField.text!, forKey: "name")
+        UserDefaults.standard.set(dayTextfield.text!, forKey: "birthday")
+        
+        
+        savedNameLabel.text = "Name:- \(nameTextField.text!)"
+        savedDayLabel.text = "Birthday:- \(dayTextfield.text!)"
+         
     }
     
 }
